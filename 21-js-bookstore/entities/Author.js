6b5e1416997bb3abef93eac142f666f825cab7eb
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2024-10-25 11:22:21 -0300

    Atualiza caminho de diretório com as principais classes do projeto prático em JS V - POO

---
+++ 21-js-bookstore/entities/Author.js
@@ -0,0 +1,8 @@
+module.exports = class Author{
+    // Classe destinada ao autor do livro
+    constructor(name, nationality, bio){
+        this.name = name
+        this.nationality = nationality
+        this.bio = bio
+    }
+}
---
+++ 21-js-bookstore/entities/User.js
@@ -0,0 +1,8 @@
+module.exports = class User{
+    // Classe destinada ao usuário (cliente)
+    constructor(name, email, password){
+        this.name = name
+        this.email = email
+        this.password = password
+    }
+}
